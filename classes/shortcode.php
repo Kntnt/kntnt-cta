5e4135db7c13d0ef2f9f365992c37511495e322c
Thomas Barregren <thomas@kntnt.se>
2020-08-30 13:16:28 +0200

    Added `groups` as optional attribute.

--- classes/shortcode.php
+++ classes/shortcode.php
@@ -4,31 +4,82 @@ namespace Kntnt\CTA;
 
 class Shortcode {
 
+    private static $defaults = [
+        'groups' => null,
+    ];
+
     public function run() {
         add_shortcode( 'cta', [ $this, 'shortcode' ] );
     }
 
     public function shortcode( $atts ) {
+
         $output = '';
-        if ( $cta_group = $this->cta_group() ) {
-            CSS::load();
-            JS::load();
-            $output = "<div class=\"kntnt-cta\" data-cta-group=\"{$cta_group}\"></div>";
+
+        // Fill in blanks with default values.
+        $atts = $this->shortcode_atts( self::$defaults, $atts );
+
+        if ( $atts['groups'] ) {
+            $cta_group = $this->randomly_pick( $atts['groups'] );
+            Plugin::log( 'Randomly selected %s CTA Group from list provided in shortcode.', $cta_group );
         }
+        else {
+            $pid = get_the_ID();
+            $groups = wp_get_object_terms( $pid, 'cta-group', [ 'fields' => 'slugs' ] );
+            $cta_group = $this->randomly_pick( $groups );
+            Plugin::log( 'Randomly selected %s CTA Group from terms on post %s.', $cta_group, $pid );
+        }
+
+        // Prepare for rendering a CTA if a CTA Group is picked and contains at
+        // least one CTA.
+        if ( $cta_group ) {
+            $term = get_term_by( 'slug', $cta_group, 'cta-group' );
+            if ( false !== $term || $term->count > 0 ) {
+                JS::load();
+                CSS::load();
+                $output = "<div class=\"kntnt-cta\" data-cta-group=\"{$cta_group}\"></div>";
+            }
+        }
+
         return $output;
+
     }
 
-    private function cta_group() {
-        $pid = get_the_ID();
-        $groups = wp_get_object_terms( $pid, 'cta-group', [ 'fields' => 'slugs' ] );
-        $group = $this->randomly_pick( $groups );
-        Plugin::log( "Randomly selected CTA Group %s for post %s.", $group, $pid );
-        return $group;
+    // A more forgiving version of WP's shortcode_atts().
+    private function shortcode_atts( $pairs, $atts, $shortcode = '' ) {
+
+        $atts = (array) $atts;
+        $out = [];
+        $pos = 0;
+        while ( $name = key( $pairs ) ) {
+            $default = array_shift( $pairs );
+            if ( array_key_exists( $name, $atts ) ) {
+                $out[ $name ] = $atts[ $name ];
+            }
+            else if ( array_key_exists( $pos, $atts ) ) {
+                $out[ $name ] = $atts[ $pos ];
+                ++ $pos;
+            }
+            else {
+                $out[ $name ] = $default;
+            }
+        }
+
+        if ( $shortcode ) {
+            $out = apply_filters( "shortcode_atts_{$shortcode}", $out, $pairs, $atts, $shortcode );
+        }
+
+        return $out;
+
     }
 
-    // Is supposedly faster than PHP's array_rand().
-    static public function randomly_pick( $array ) {
-        return $array[ mt_rand( 0, count( $array ) - 1 ) ];
+    // Randomly pick an element from an array or a comma separated list.
+    // Using mt_rand() since it allegedly is faster than array_rand().
+    static private function randomly_pick( $list ) {
+        if ( is_string( $list ) ) {
+            $list = explode( ',', $list );
+        }
+        return is_array( $list ) && ! empty( $list ) ? $list[ mt_rand( 0, count( $list ) - 1 ) ] : null;
     }
 
 }
